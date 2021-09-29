3fd8c4b2552a860adc9c07ddf87b2860a316d604
studentTA <66950149+studentTA@users.noreply.github.com>
2021-09-29 08:26:55 +0200

    Add files via upload

---
+++ catpoint-parent/Security/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module SecurityModule {
+    requires transitive java.desktop;
+    requires transitive java.prefs;
+    requires transitive java.sql;
+    requires transitive miglayout.swing;
+    requires transitive gson;
+    requires transitive com.google.common;
+    requires transitive ImageModule;
+    opens com.udacity.catpoint.security.data to com.google.gson;
+    opens com.udacity.catpoint.security.service to com.google.gson;
+}
